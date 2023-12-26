793cca157c40c897c0c9328397a64a98ddc5a91f
mjalen <ajalenboi@gmail.com>
2023-12-26 01:35:38 -0600

    .

--- home/jalen.nix
+++ home/jalen.nix
@@ -2,15 +2,16 @@
     home.username = "jalen";
     home.homeDirectory = "/home/jalen";
 
+    home.packages = with pkgs; [
+        ranger
+        neofetch
+    ];
+
     programs.git = {
         enable = true;
         userName = mjalen; 
         userEmail = "ajalenboi@gmail.com"; # email me [ at your own peril >:) ]
     };
-
-    home.packages = with pkgs; head [
-        ranger
-    ];
 
     programs.neovim = {
         enable = lib.mkDefault true;
