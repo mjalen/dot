2185b89f71b3be5f4453795befc802dad988fe22
mjalen <ajalenboi@gmail.com>
2024-02-02 00:40:08 -0600

    Updated README and imported emacs config over.

--- home/applications/emacs/emacs.d/init.el
+++ home/applications/emacs/emacs.d/init.el
@@ -39,21 +39,3 @@
  (expand-file-name
   "config.org"
   user-emacs-directory))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   '("6f96a9ece5fdd0d3e04daea6aa63e13be26b48717820aa7b5889c602764cf23a"
-     "a44e2d1636a0114c5e407a748841f6723ed442dc3a0ed086542dc71b92a87aee"
-     "b1acc21dcb556407306eccd73f90eb7d69664380483b18496d9c5ccc5968ab43"
-     "1f292969fc19ba45fbc6542ed54e58ab5ad3dbe41b70d8cb2d1f85c22d07e518"
-     "570263442ce6735821600ec74a9b032bc5512ed4539faf61168f2fdf747e0668"
-     default)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
